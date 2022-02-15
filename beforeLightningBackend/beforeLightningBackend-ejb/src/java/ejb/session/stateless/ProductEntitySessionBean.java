3d5cba2a4967889c602f2ea72678121f46e01b95
irene <ireneliee>
2022-02-15 11:22:18 +0800

    createNewProduct

--- beforeLightningBackend/beforeLightningBackend-ejb/src/java/ejb/session/stateless/ProductEntitySessionBean.java
+++ beforeLightningBackend/beforeLightningBackend-ejb/src/java/ejb/session/stateless/ProductEntitySessionBean.java
@@ -5,12 +5,22 @@
  */
 package ejb.session.stateless;
 
+import entity.PartChoiceEntity;
+import entity.PartEntity;
 import entity.ProductEntity;
+import java.util.List;
+import java.util.Set;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import util.exception.CreateNewProductEntityException;
 import util.exception.InputDataValidationException;
+import util.exception.ProductSkuCodeExistException;
 import util.exception.UnknownPersistenceException;
 
 /**
@@ -22,22 +32,83 @@ public class ProductEntitySessionBean implements ProductEntitySessionBeanLocal {
 
     @PersistenceContext(unitName = "beforeLightningBackend-ejbPU")
     private EntityManager entityManager;
-    
+
+    private final ValidatorFactory validatorFactory;
+    private final Validator validator;
+
     public ProductEntitySessionBean() {
-    }
-    
-    
-    public ProductEntity createNewProduct(ProductEntity newProduct) throws CreateNewProductEntityException, InputDataValidationException,
-            UnknownPersistenceException {
-        // not done yet
-        // create new Product here 
-        // need to persist the part & part choices here as well
-        // persist product first (without part and part choices, then persist part, then persist part choices)
-        
-        return null;
-    
+
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+
     }
 
-    
-    
+    // assuming parts and part choices will only be created tgt
+    public ProductEntity createNewProduct(ProductEntity newProductEntity) throws CreateNewProductEntityException, InputDataValidationException,
+            UnknownPersistenceException, ProductSkuCodeExistException {
+
+        Set<ConstraintViolation<ProductEntity>> constraintViolations = validator.validate(newProductEntity);
+
+        if (constraintViolations.isEmpty()) {
+
+            try {
+
+                List<PartEntity> listOfPartEntities = newProductEntity.getParts();
+
+                for (PartEntity p : listOfPartEntities) {
+
+                    List<PartChoiceEntity> listOfPartChoices = p.getPartChoices();
+
+                    for (PartChoiceEntity pc : listOfPartChoices) {
+
+                        entityManager.persist(pc);
+
+                    }
+
+                    entityManager.persist(p);
+
+                }
+
+                entityManager.persist(newProductEntity);
+
+                entityManager.flush();
+
+                return newProductEntity;
+
+            } catch (PersistenceException ex) {
+
+                if (ex.getCause() != null && ex.getCause().getClass().getName().equals("org.eclipse.persistence.exceptions.DatabaseException")) {
+                    
+                    if (ex.getCause().getCause() != null && ex.getCause().getCause().getClass().getName().equals("java.sql.SQLIntegrityConstraintViolationException")) {
+
+                        throw new ProductSkuCodeExistException();
+
+                    } else {
+                        throw new UnknownPersistenceException(ex.getMessage());
+                    }
+                } else {
+
+                    throw new UnknownPersistenceException(ex.getMessage());
+
+                }
+            }
+
+        } else {
+
+            throw new InputDataValidationException(prepareInputDataValidationErrorsMessage(constraintViolations));
+
+        }
+
+    }
+
+    private String prepareInputDataValidationErrorsMessage(Set<ConstraintViolation<ProductEntity>> constraintViolations) {
+        String msg = "Input data validation error!:";
+
+        for (ConstraintViolation constraintViolation : constraintViolations) {
+            msg += "\n\t" + constraintViolation.getPropertyPath() + " - " + constraintViolation.getInvalidValue() + "; " + constraintViolation.getMessage();
+        }
+
+        return msg;
+    }
+
 }
