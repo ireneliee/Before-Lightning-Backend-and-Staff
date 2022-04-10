487eea14262257d663eeede4c7252136cd007717
Koh Wen Jie <17scwenjie@gmail.com>
2022-04-10 16:47:07 +0800

    update AccessoryResouce RWS

--- beforeLightningRWS/src/java/ws/restful/AccessoryResource.java
+++ beforeLightningRWS/src/java/ws/restful/AccessoryResource.java
@@ -10,10 +10,14 @@ import entity.PartChoiceEntity;
 import entity.PartEntity;
 import entity.AccessoryEntity;
 import entity.AccessoryItemEntity;
+import entity.ProductEntity;
 import entity.PromotionEntity;
+import java.util.ArrayList;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
@@ -55,7 +59,7 @@ public class AccessoryResource {
                     System.out.println("here 2");
                     accessoryItem.setAccessoryEntity(null);
                     System.out.println("here 3");
-                    
+
                     for (PromotionEntity promo : accessoryItem.getPromotionEntities()) {
                         System.out.println("here 4");
                         promo.getAccessoryItemEntities().clear();
@@ -72,6 +76,45 @@ public class AccessoryResource {
             };
 
             return Response.status(Response.Status.OK).entity(genericEntity).build();
+        } catch (Exception ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+
+    @Path("retrieveAccessoryToSellById/{accessoryId}")
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveAccessoryToSellById(@PathParam("accessoryId") Long accessoryId) {
+        try {
+            System.out.println(" ============== RETRIEVE Accessory TO SELL ===============");
+            List<AccessoryEntity> accessoryEntities = accessoryEntitySessionBeanLocal.retrieveAllAccessoryEntitiesNotDisabled();
+            System.out.println("PROCESSING SIZE: " + accessoryEntities.size());
+
+            // =============== UNMARSHALLING =============
+            for (AccessoryEntity accessory : accessoryEntities) {
+                System.out.println("here");
+                if (accessory.getAccessoryEntityId().equals(accessoryId)) {
+                    for (AccessoryItemEntity accessoryItem : accessory.getAccessoryItemEntities()) {
+                        System.out.println("here 2");
+                        accessoryItem.setAccessoryEntity(null);
+                        System.out.println("here 3");
+
+                        for (PromotionEntity promo : accessoryItem.getPromotionEntities()) {
+                            System.out.println("here 4");
+                            promo.getAccessoryItemEntities().clear();
+                            promo.getPartChoiceEntities().clear();
+                            System.out.println("here 5");
+                        }
+                        System.out.println("hereeeee");
+                    }
+                    System.out.println("SENDING ACCESSORY: ");
+                    System.out.println(accessory);
+                    return Response.status(Response.Status.OK).entity(accessory).build();
+                }
+            }
+
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("UNABLE TO FIND ACCESSORY").build();
         } catch (Exception ex) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
         }
